4aa724cf224ce7029926eef665b26097e9b884f0
ElTinmar <martin.privat@hotmail.fr>
2025-01-29 10:18:11 +0100

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ViewSonicProjectorRS232',
+    python_requires='>=3.8',
+    author='Martin Privat',
+    version='0.1.0',
+    packages=[''],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Control Viewsonic pojector over serial',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pyserial",
+    ]
+)
